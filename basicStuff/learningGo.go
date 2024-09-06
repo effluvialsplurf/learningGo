5f6dc7e96deb6ab793f652c86a6533a8fa79210b
effluvialsplurf <eesowrk@gmail.com>
2024-09-06 00:21:09 -0400

    added a type exploration to the basics file

--- basicStuff/learningGo.go
+++ basicStuff/learningGo.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
+func exploringStructs() {
+	// struct
+	type Pokemon struct {
+		name    string
+		hp      int
+		attacks []string
+	}
+
+	pichu := Pokemon{}
+	pichu.name = "Pichu"
+	pichu.hp = 31
+	pichu.attacks = []string{"volt tackle", "tail whip"}
+
+	// anonymous struct
+	magicCard := struct {
+		name  string
+		stars int
+		stats map[string]int
+	}{
+		name:  "Winged Kuriboh",
+		stars: 1,
+		stats: map[string]int{
+			"attack":  300,
+			"defense": 200,
+		},
+	}
+
+	fmt.Printf("pokemon: %v, and then magicCard: %v\n", pichu, magicCard)
+}
 
 func main() {
-  fmt.Println("Finally, a git project AND a hello world file :)")
+	fmt.Println("Finally, a git project AND a hello world file :)")
+	exploringStructs()
 }
