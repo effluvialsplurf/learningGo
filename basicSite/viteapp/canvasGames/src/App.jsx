df936f89004bf3f7fef020235d227ccdf96c7161
__eepy__(Oliver-wsl) <eesowrk@gmail.com>
2024-12-21 00:54:42 -0500

    made the initial snake game and set the app to be the snake game

--- basicSite/viteapp/canvasGames/src/App.jsx
+++ basicSite/viteapp/canvasGames/src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function randint(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef();
+  const [snakeList, setSnakeList] = useState([{x: 100, y: 110}]);
+  const [foodPos, setFoodPos] = useState({x: randint(20, 980), y: randint(20, 680)});
+  const [direction, setDirection] = useState('right');
+  const [score, setScore] = useState(0);
+
+  const nodeLength = 15;
+  const nodeHeight = 15;
+  
+  useEffect(() => {
+    const gameLoop = setInterval(() => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+
+      // canvas dimensions
+      canvas.width = 1000;
+      canvas.height = 700;
+
+      // canvas background
+      ctx.fillStyle = 'black';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      // do snake game stuff
+
+      // draw snake
+      ctx.beginPath();
+      ctx.rect(30, 40, nodeLength, nodeHeight);
+      ctx.fillStyle = 'purple';
+      ctx.fill();
+      ctx.closePath();
+
+      // draw food
+      ctx.beginPath();
+      ctx.rect(foodPos.x, foodPos.y, nodeLength, nodeHeight);    
+      ctx.fillStyle = 'red';
+      ctx.fill();
+      ctx.closePath();
+      }, 1000 / 60);
+  }, [snakeList, direction, foodPos, score]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="canvasContainer">
+      <canvas ref={canvasRef} />
+    </div>
   )
 }
 
